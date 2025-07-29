51ec55e4e22c24747a654c250f07090813429cb0
Eric Chen <ericchen87887@gmail.com>
2025-07-29 15:49:40 +0800

    add stun server

--- main.js
+++ main.js
@@ -2,6 +2,15 @@ let localStream;
 let remoteStream;
 let peerConnection;
 
+
+const servers = {
+    iceServers:[
+        {
+            urls:["stun:stun1.1.google.com:19302", "stun:stun2.1.google.com:19302"]
+        }
+    ]
+}
+
     // get the cam and show it on screen
 let init = async() =>{
 
@@ -13,6 +22,9 @@ let init = async() =>{
     // 把取得的本地串流（localStream）設定到 HTML 中 id 為 "user-1" 的 video 元素
     // 這樣就可以在畫面上看到自己的影像
     document.getElementById("user-1").srcObject = localStream;
+
+
+    createOffer()
 }
 
 
@@ -22,7 +34,7 @@ let init = async() =>{
     // 建立一個 async 函式，負責創建 WebRTC offer，發起端會呼叫這個函式
 let createOffer = async() => {
 
-    peerConnection = new RTCPeerConnection()
+    peerConnection = new RTCPeerConnection(servers)
 
     // 建立一個空的 MediaStream，準備拿來裝「對方傳過來」的影像/音訊串流
     remoteStream = new MediaStream()
@@ -35,6 +47,9 @@ let createOffer = async() => {
 
     // 這是建立 WebRTC 連線的第一步（之後會把這個 offer 傳給遠端對等）
     await peerConnection.setLocalDescription(offer)
+
+
+    console.log("offer:", offer)
     
 }
 
